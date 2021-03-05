fbfd5408594471a4913612cd454e2c56ec4d7c3c
Muras.Wojciech <215828@edu.p.lodz.pl>
2021-03-05 17:13:29 +0100

    code formatting

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -8,7 +8,7 @@ public class TailsGenerator {
     public List<String> tails(String value) {
 
         List<String> tails = new ArrayList<String>();
-        for(int i=0; i<=value.length();i++){
+        for (int i = 0; i <= value.length(); i++) {
             tails.add(value.substring(i));
         }
         return tails;
