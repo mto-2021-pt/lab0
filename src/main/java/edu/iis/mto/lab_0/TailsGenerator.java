7bc0eeed566ca3c257af90ad25cc1cf15891c1e9
Pluta.Radoslaw <222541@edu.p.lodz.pl>
2021-03-05 17:37:19 +0100

    Corrected function tails.

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,19 +6,10 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        ArrayList<String> ret = new ArrayList<>(value.length() + 1);
+        List<String> ret = new ArrayList<>(value.length() + 1);
         for (int i = 0; i < value.length() + 1; ++i) {
-            if (i == 2) {
-                ret.add(value.substring(2));
-            }
-            else if (i == value.length()) {
-                ret.add("");
-            }
-            else {
-                ret.add(value);
-            }
+            ret.add(value.substring(i));
         }
-
         return ret;
     }
 
