92afc562a0fbee2b6d760a274f9e55f199dca0d2
Pluta.Radoslaw <222541@edu.p.lodz.pl>
2021-03-05 17:26:45 +0100

    Implemented tests.

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,20 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        ArrayList<String> ret = new ArrayList<>(value.length() + 1);
+        for (int i = 0; i < value.length() + 1; ++i) {
+            if (i == 2) {
+                ret.add(value.substring(2));
+            }
+            else if (i == value.length()) {
+                ret.add("");
+            }
+            else {
+                ret.add(value);
+            }
+        }
+
+        return ret;
     }
 
 }
