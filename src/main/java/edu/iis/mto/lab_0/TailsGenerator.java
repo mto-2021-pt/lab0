804a0a3e0616838527f6757e758901a4e69a3cb4
mechecka.olga <222508@edu.p.lodz.pl>
2021-03-05 18:30:05 +0100

    usuniecie plikow konfiguracyjnych

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TailsGenerator {
+
     public List<String> tails(String value) {
         ArrayList<String> list = new ArrayList<>(value.length());
         for(int i=0; i<=value.length(); i++){
