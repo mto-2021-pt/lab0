cfa3018b94874447606cc18ce36be806621d8cb0
mechecka.olga <222508@edu.p.lodz.pl>
2021-03-05 17:47:56 +0100

    poprawa metody lists

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -5,8 +5,14 @@ import java.util.List;
 
 public class TailsGenerator {
 
+
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+
+        ArrayList<String> list = new ArrayList<>(value.length());
+        for(int i=0; i<=value.length(); i++){
+            list.add(value.substring(i));
+        }
+        return list;
     }
 
 }
