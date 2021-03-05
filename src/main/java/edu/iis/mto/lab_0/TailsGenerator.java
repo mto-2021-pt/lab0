2029dc3020ac7c14cddab3fbf9cb2083fc041809
mechecka.olga <222508@edu.p.lodz.pl>
2021-03-05 18:08:51 +0100

    poprawka22

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-
         ArrayList<String> list = new ArrayList<>(value.length());
         for(int i=0; i<=value.length(); i++){
             list.add(value.substring(i));
